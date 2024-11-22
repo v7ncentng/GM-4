38c217bcbdf3f12786609f9f7f825082457f6557
v7ncentng <128509488+v7ncentng@users.noreply.github.com>
2024-11-21 23:45:13 -0800

    Create workshop.js

---
+++ workshop.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('linkedinButton').addEventListener('click', function () {
+        window.location.href = 'https://www.linkedin.com/in/wang-ng-8805b2213/';
+    });
+
+    document.getElementById('githubButton').addEventListener('click', function () {
+        window.location.href = 'https://github.com/v7ncentng';
+    });
+});
